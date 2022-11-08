d6e9e946da378ff9b6b3288c940ce6f9ad90629a
RJ <RJ@RJs-MacBook-Pro.local>
2022-11-08 11:29:34 -0800

    Replaced .dbc files with .py files

---
+++ event_hub/eh-hl7-structure-ok.py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+import json, os
+ev_namespace    ="tf-eventhub-namespace-dev"
+ev_name         ="hl7-structure-ok"
+ev_sas_key_name = os.getenv("v_hl7_structure_ok_key")
+ev_sas_key_val = os.getenv("v_hl7_structure_ok_key_val")
+
+conn_string="Endpoint=sb://{0}.servicebus.windows.net/;EntityPath={1};SharedAccessKeyName={2};SharedAccessKey={3}".format(ev_namespace, ev_name, ev_sas_key_name, ev_sas_key_val)
+
+ehConf = {}
+ehConf['eventhubs.connectionString'] = sc._jvm.org.apache.spark.eventhubs.EventHubsUtils.encrypt(conn_string)
+print(conn_string)
+
+# COMMAND ----------
+
+##### Creating an Event Hubs Source for Streaming Queries
+df = spark.readStream.format("eventhubs").options(**ehConf).load()
+df = df.withColumn("body", df["body"].cast("string"))
+#df.printSchema()
+
+# COMMAND ----------
+
+db_name ="ocio_ede_dev"
+tbl_name = "tbl_hl7_structure_ok"
+schema_name = db_name + "." + tbl_name
+chkpoint_loc = "/tmp/delta/events/hl7_structure_ok/_checkpoints/"
+
+#df.writeStream.outputMode("append").format("console").option("truncate", false).start().awaitTermination()
+df.writeStream.format("delta").outputMode("append").option("checkpointLocation", chkpoint_loc).toTable(schema_name)
+#df.printSchema()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT count(*) FROM ocio_ede_dev.tbl_hl7_structure_ok;
---
+++ event_hub/eh_hl7_file_dropped.py
@@ -0,0 +1,39 @@
+# Databricks notebook source
+import json, os
+ev_namespace    ="tf-eventhub-namespace-dev"
+ev_name         ="hl7-file-dropped"
+ev_sas_key_name = os.getenv("v_hl7_file_dropped_key")
+ev_sas_key_val = os.getenv("v_hl7_file_dropped_key_val")
+
+conn_string="Endpoint=sb://{0}.servicebus.windows.net/;EntityPath={1};SharedAccessKeyName={2};SharedAccessKey={3}".format(ev_namespace, ev_name, ev_sas_key_name, ev_sas_key_val)
+
+ehConf = {}
+ehConf['eventhubs.connectionString'] = sc._jvm.org.apache.spark.eventhubs.EventHubsUtils.encrypt(conn_string)
+#print(conn_string)
+
+# COMMAND ----------
+
+##### Creating an Event Hubs Source for Streaming Queries
+df = spark.readStream.format("eventhubs").options(**ehConf).load()
+df = df.withColumn("body", df["body"].cast("string"))
+#df.printSchema()
+
+# COMMAND ----------
+
+db_name ="ocio_ede_dev"
+tbl_name = "tbl_hl7_file_dropped"
+schema_name = db_name + "." + tbl_name
+chkpoint_loc = "/tmp/delta/events/hl7_file_dropped/_checkpoints/"
+
+df.writeStream.format("delta").outputMode("append").option("checkpointLocation", chkpoint_loc).toTable(schema_name)
+#df.writeStream.format("delta").outputMode("append").option("checkpointLocation", "/tmp/delta/events/_checkpoints/").start("/delta/events")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT Count(*) FROM ocio_ede_dev.tbl_hl7_file_dropped;
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC --Drop table ocio_ede_dev.tbl_hl7_file_dropped;
