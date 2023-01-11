2a9616c7037be04caff1e8d86a573f3275ed0330
cosmycx <cosmycx>
2023-01-11 17:38:25 +0000

    mmg-sql and mmg-based models show test

--- pipeline/dev_tests/lrp4-mmg-based-test.py
+++ pipeline/dev_tests/lrp4-mmg-based-test.py
@@ -1,13 +1,13 @@
 # Databricks notebook source
 
-mmg_sql_model = spark.sql("SELECT * FROM ocio_dex_dev.hl7_mmg_sql_ok_eh_raw")
+mmg_sql_model = spark.sql("SELECT * FROM ocio_dex_dev.hl7_mmg_based_ok_eh_raw")
 
 display(mmg_sql_model)
 
 # COMMAND ----------
 
 
-df1 = spark.sql("SELECT * FROM ocio_dex_dev.hl7_mmg_sql_ok_eh_raw sort by enqueuedTime desc")
+df1 = spark.sql("SELECT * FROM ocio_dex_dev.hl7_mmg_based_ok_eh_raw sort by enqueuedTime desc")
 display(df1)
 
 
@@ -16,7 +16,7 @@ display(df1)
 
 
 
-df2 = spark.sql("select body:metadata:processes[4]:report from ocio_dex_dev.hl7_mmg_sql_ok_eh_raw ")
+df2 = spark.sql("select body:metadata:processes[3]:report from ocio_dex_dev.hl7_mmg_based_ok_eh_raw ")
 display(df2)
 
 
@@ -27,14 +27,13 @@ import json
 from pyspark.sql import functions as F
 
 from pyspark.sql.functions import get_json_object
-saved  = spark.sql("select body:message_uuid, body:metadata:processes[4]:report from ocio_dex_dev.hl7_mmg_sql_ok_eh_raw")
+saved  = spark.sql("select body:message_uuid, body:metadata:processes[3]:report from ocio_dex_dev.hl7_mmg_based_ok_eh_raw")
 
 bodycollect = saved.collect()
 
 all_keys = []
 for entry in bodycollect:
-    if entry[1]:
-        dictionary = json.loads(entry[1])
+    dictionary = json.loads(entry[1])
     
     for key in dictionary.keys():
         if not key in all_keys:
@@ -43,7 +42,7 @@ for entry in bodycollect:
 '/**display(len(all_keys))'
 df = saved.select('message_uuid',F.json_tuple('report', *all_keys)).toDF('message_uuid',*all_keys)
 
-'/** df2 = df.filter("message_uuid == b1ddfc1a-8add-40be-ad4f-99ed0f6bdf25")'
+'/** df2 = df.filter("message_uuid == 0332acec-4f68-4842-9bbd-49287d72d4ba")'
 row = df.count()
  
 print(f'Number of Rows:  + {row}')
@@ -56,11 +55,6 @@ display(df)
 dff = df.where("message_uuid == 'b1ddfc1a-8add-40be-ad4f-99ed0f6bdf25'")
 
 display(dff)
-
-# COMMAND ----------
-
-display(dff.select("tables"))
-
 
 # COMMAND ----------
 
