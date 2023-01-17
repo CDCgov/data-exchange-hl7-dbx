533e24c2ec3b3e0e6d671a0d9834c53acb383188
dtx0@cdc.gov <dtx0@cdc.gov>
2023-01-17 21:53:31 +0000

    updated columns as per bronze table

--- pipeline/silver/eh_hl7_structure_err_silver (1).py
+++ pipeline/silver/eh_hl7_structure_err_silver (1).py
@@ -7,7 +7,7 @@
  source_db = "ocio_dex_dev"
  target_tbl_name = "hl7_structure_err_silver"
  target_schema_name = source_db + "." + target_tbl_name
- chkpoint_loc = "abfss://ocio-dex-db-dev@ocioededatalakedbr.dfs.core.windows.net/delta/events/" + target_tbl_name + "/_checkpoint4"
+ chkpoint_loc = "abfss://ocio-dex-db-dev@ocioededatalakedbr.dfs.core.windows.net/delta/events/" + target_tbl_name + "/_checkpoint5"
 
 
 df =  spark.readStream.format("delta").table("ocio_dex_dev.hl7_structure_err_bronze")
@@ -19,12 +19,12 @@ from datetime import datetime
 
 from pyspark.sql import functions as F
 
-df2 = df.select('message_uuid', 'route', 'metadata.provenance.message_hash', 'metadata.provenance.message_index','metadata.provenance.event_timestamp', 'metadata.provenance.ext_original_file_name', 'report.entries.content','errorCount' )
+df2 = df.select('message_uuid', 'message_info.route', 'message_hash', 'provenance.message_index','process_start_time', 'process_end_time', 'provenance.ext_original_file_name', 'report.entries.content','errorCount' )
 
 
 df3 = df2.withColumn('content', F.explode('content'))
 
-df4 = df3.select('message_uuid','route', 'message_hash', 'message_index', 'event_timestamp', 'ext_original_file_name', 'content.line','content.column','content.path','content.description','content.category','content.stacktrace')
+df4 = df3.select('message_uuid','route', 'message_hash', 'message_index', 'process_start_time', 'process_end_time','ext_original_file_name', 'content.line','content.column','content.path','content.description','content.category','content.stacktrace')
 
 display(df4)
 
