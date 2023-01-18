ee3cf9aa3f34185b8eb65633794dcabfbadebc51
quq6@cdc.gov <quq6@cdc.gov>
2023-01-18 22:04:53 +0000

    complete writing to table

--- pipeline/bronze/eh_hl7_create_target.py
+++ pipeline/bronze/eh_hl7_create_target.py
@@ -20,6 +20,12 @@ target_table = dbutils.widgets.get("target_table")
 
 # COMMAND ----------
 
+# process name (the report to extract from processes metadata)
+dbutils.widgets.dropdown("process_name", "mmgBasedTransformer", ["RECEIVER", "MMG-VALIDATOR", "mmgBasedTransformer"] )
+process_name = dbutils.widgets.get("process_name")
+
+# COMMAND ----------
+
 src_schema_name = f"{source_database}.{source_table}"
 chkpoint_loc = f"abfss://ocio-dex-db-dev@ocioededatalakedbr.dfs.core.windows.net/delta/events/{target_table}/_checkpoint"
 
@@ -53,13 +59,13 @@ processSchema = StructType([ StructField("process_name", StringType(), True), \
 
 schema = StructType([ StructField("content", StringType(), True), \
     StructField("message_uuid", StringType(), True), \
-    StructField("message_hash", StringType(), True), \
     StructField("metadata", StructType(
             [ StructField("provenance", 
                           StructType(
                               [ StructField("file_path", StringType(), True), \
                                 StructField("file_timestamp", StringType(), True), \
                                 StructField("file_size", LongType(), True), \
+                                StructField("message_hash", StringType(), True), \
                                 StructField("single_or_batch", StringType(), True) ]), True), \
               StructField("processes", ArrayType(processSchema, True), True) ]), True), \
 
@@ -74,21 +80,24 @@ schema = StructType([ StructField("content", StringType(), True), \
 
 # COMMAND ----------
 
-from pyspark.sql.functions import from_json, col, get_json_object
+from pyspark.sql.functions import *
 df1 = df_source.withColumn("bodyJson", from_json(col("body"), schema))
 df_body = df1.select("bodyJson.*")
 display(df_body)
 
 # COMMAND ----------
 
-df_body.printSchema()
-df3 = df_body.withColumn("processes", df_body.metadata.processes)
+df3 = df_body.withColumn("processes", df_body.metadata.processes).withColumn("provenance", df_body.metadata.provenance).withColumn("message_hash", df_body.metadata.provenance.message_hash)
 display(df3)
 
 # COMMAND ----------
 
-dbutils.help()
+df4 = df3.withColumn("mmgReport", explode(df3.processes)).filter( f"mmgReport.process_name == '{process_name}'").withColumn("process_version", col("mmgReport.process_version")).withColumn("process_status", col("mmgReport.status")).withColumn("process_start_time", col("mmgReport.start_processing_time")).withColumn("process_end_time", col("mmgReport.end_processing_time")).withColumn("report", col("mmgReport.report"))
+display( df4 )
 
 # COMMAND ----------
 
-dbutils.widgets.help()
+# Creating a Target Bronze table in the Database.
+target_schema_name = f"{target_database}.{target_table}"
+
+df4.writeStream.format("delta").outputMode("append").option("checkpointLocation", chkpoint_loc).toTable(target_schema_name)
