f0ab876fa19f29d0c1a261c0d7145efb157e7788
dtx0@cdc.gov <dtx0@cdc.gov>
2023-03-02 17:04:08 +0000

    Fixed indentation

--- pipeline/eventhub2DeltaLake/eh_hl7_redacted_ok_2deltalake.py
+++ pipeline/eventhub2DeltaLake/eh_hl7_redacted_ok_2deltalake.py
@@ -6,8 +6,3 @@
 ##### Stream Eventhub data to to Delta Lake
 eventHubTopic = "hl7-redacted-ok"
 transferEventHubDataToLake(eventHubTopic)
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select * from ocio_dex_dev.hl7_redacted_ok_eh_raw
