5ed93fea59e02994716893e9239ebf16586a7d54
mscaldas2012 <mscaldas2012>
2023-03-09 21:35:08 +0000

    merge schema = true for gold tables

--- pipeline/common/common_fns.py
+++ pipeline/common/common_fns.py
@@ -11,11 +11,11 @@ dbutils.widgets.dropdown("eventhub_namespace", "tf-eventhub-namespace-dev", ["tf
 dbutils.widgets.dropdown("scope_name", "DBS-SCOPE-DEX-DEV", ["DBS-SCOPE-DEX-DEV"])
 dbutils.widgets.dropdown("database", "ocio_dex_dev", ["ocio_dex_dev"])
 dbutils.widgets.dropdown("database_checkpoint_prefix", "abfss://ocio-dex-db-dev@ocioededatalakedbr.dfs.core.windows.net/delta/checkpoints", ["abfss://ocio-dex-db-dev@ocioededatalakedbr.dfs.core.windows.net/delta/checkpoints"])
-dbutils.widgets.dropdown("database_folder", "abfss://ocio-dex-db-dev@ocioededatalakedbr.dfs.core.windows.net/delta/", ["abfss://ocio-dex-db-dev@ocioededatalakedbr.dfs.core.windows.net/delta/"])
+#dbutils.widgets.dropdown("database_folder", "abfss://ocio-dex-db-dev@ocioededatalakedbr.dfs.core.windows.net/delta/", ["abfss://ocio-dex-db-dev@ocioededatalakedbr.dfs.core.windows.net/delta/"])
 
 #
 ####### this can be used if final gold moves to Edav, etc..
-dbutils.widgets.dropdown("gold_output_database", "ocio_dex_dev", ["ocio_dex_dev"])
+dbutils.widgets.dropdown("gold_output_database", "ocio_dex_prog_dev", ["ocio_dex_prog_dev"])
 dbutils.widgets.dropdown("gold_output_database_checkpoint_prefix", "abfss://ocio-dex-db-dev@ocioededatalakedbr.dfs.core.windows.net/delta/checkpoints", ["abfss://ocio-dex-db-dev@ocioededatalakedbr.dfs.core.windows.net/delta/checkpoints"])
 
 # COMMAND ----------
@@ -23,7 +23,7 @@ dbutils.widgets.dropdown("gold_output_database_checkpoint_prefix", "abfss://ocio
 eventhub_namespace =  dbutils.widgets.get("eventhub_namespace")
 database =  dbutils.widgets.get("database")
 database_checkpoint_prefix = dbutils.widgets.get("database_checkpoint_prefix")
-database_folder = dbutils.widgets.get("database_folder")
+#database_folder = dbutils.widgets.get("database_folder")
 
 scope_name= dbutils.widgets.get("scope_name")
 gold_output_database =  dbutils.widgets.get("gold_output_database")
@@ -97,23 +97,20 @@ class TableConfig:
     def output_database_table(self):
         return f"{self.database_config.database}.{self.topic}_{self.stage_out}"
     
-    def output_checkpoint(self):
-        return f"{self.database_config.database_checkpoint_prefix}/{self.topic}_{self.stage_out}_checkpoint"   
-      
     def output_gold_table(self, program_route):
         return f"{self.database_config.gold_output_database}.{normalize(program_route)}_{self.topic}_gold"
     
-    def output_gold_table_checkpoint(self, program_route):
-        output_gold_tbl = f"{normalize(program_route)}_{self.topic}"
-        return  f"{self.database_config.gold_output_database_checkpoint_prefix}/{output_gold_tbl}_gold_checkpoint" 
-      
     def output_gold_repeat_table(self, program_route, repeat_table):
-        gold_tbl = f"{self.database_config.gold_output_database}.{normalize(program_route)}_{repeat_table}_{self.topic}"
-        return f"{gold_tbl}_gold"
-    
+        return f"{self.database_config.gold_output_database}.{normalize(program_route)}_{repeat_table}_{self.topic}_gold"
+        
+    def output_checkpoint(self):
+        return f"{self.database_config.database_checkpoint_prefix}/{self.topic}_{self.stage_out}_checkpoint"   
+      
+    def output_gold_table_checkpoint(self, program_route):
+        return  f"{self.database_config.gold_output_database_checkpoint_prefix}/{normalize(program_route)}_{self.topic}_gold_checkpoint" 
+      
     def output_gold_repeat_table_checkpoint(self, program_route, repeat_table):
-        gold_tbl = f"{normalize(program_route)}_{repeat_table}_{self.topic}"
-        return f"{self.database_config.gold_output_database_checkpoint_prefix}/{gold_tbl}_gold_checkpoint" 
+        return f"{self.database_config.gold_output_database_checkpoint_prefix}/{normalize(program_route)}_{repeat_table}_{self.topic}_gold_checkpoint" 
 
 
 # COMMAND ----------
@@ -142,13 +139,13 @@ class LakeUtil:
         df.writeStream.format("delta").outputMode("append").option("mergeSchema", "true").trigger(availableNow=True).option("checkpointLocation", self.table_config.output_checkpoint() ).toTable( self.table_config.output_database_table() )
     
     def write_gold_to_table(self, df, program_route):
-        df.write.format("delta").mode("append") \
+        df.write.format("delta").mode("append").option("mergeSchema", "true").trigger(availableNow=True) \
         .option("checkpointLocation", self.table_config.output_gold_table_checkpoint(program_route) ) \
         .saveAsTable( self.table_config.output_gold_table(program_route) )
     
     def write_gold_repeat_to_table(self, df, program_route, repeat_table):
         #TODO determine if checkpoints are needed here, or should move to the writeStream in the notebooks which should be configured then.
-        df.write.format("delta").mode("append") \
+        df.write.format("delta").mode("append").option("mergeSchema", "true").trigger(availableNow=True) \
         .option("checkpointLocation", self.table_config.output_gold_repeat_table_checkpoint(program_route, repeat_table) ) \
         .saveAsTable( self.table_config.output_gold_repeat_table(program_route, repeat_table) )
        
@@ -167,8 +164,8 @@ def getTableStream(database_config,tbl_name):
 
 # COMMAND ----------
 
-    # used for raw tables
-    def writeStreamToTable(database_config,tbl_name,df):
-        checkpt = f"{database_config.database_checkpoint_prefix}/{tbl_name}_checkpoint"
-        dbname = database_config.database+"."+tbl_name 
-        df.writeStream.format("delta").outputMode("append").trigger(availableNow=True).option("checkpointLocation", checkpt).toTable(dbname)
+# used for raw tables
+def writeStreamToTable(database_config,tbl_name,df):
+    checkpt = f"{database_config.database_checkpoint_prefix}/{tbl_name}_checkpoint"
+    dbname = database_config.database+"."+tbl_name 
+    df.writeStream.format("delta").outputMode("append").trigger(availableNow=True).option("checkpointLocation", checkpt).toTable(dbname)
