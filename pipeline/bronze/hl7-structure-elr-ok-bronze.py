51d543daf2e113636fa7a2d710935179e4339539
dtx0@cdc.gov <dtx0@cdc.gov>
2023-04-12 17:49:31 +0000

    Removed unwanted cells

--- pipeline/bronze/hl7-structure-elr-ok-bronze.py
+++ pipeline/bronze/hl7-structure-elr-ok-bronze.py
@@ -11,8 +11,3 @@ PROCESS_NAME = "STRUCTURE-VALIDATOR"
 
 structure_ok  = create_structure_validator_df(TOPIC, PROCESS_NAME)
 create_bronze_table(TOPIC, structure_ok)
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select * from ocio_dex_dev.hl7_structure_elr_ok_bronze
--- pipeline/eventhub2DeltaLake/eh_hl7_structure_elr_ok_2deltalake.py
+++ pipeline/eventhub2DeltaLake/eh_hl7_structure_elr_ok_2deltalake.py
@@ -6,8 +6,3 @@
 ##### Stream Eventhub data to to Delta Lake
 eventHubTopic = "hl7-structure-elr-ok"
 transferEventHubDataToLake(eventHubTopic)
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select * from ocio_dex_dev.hl7_structure_elr_ok_eh_raw
