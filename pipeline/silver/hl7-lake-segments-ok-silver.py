2715b3e123299601911a894afa1ffbc7f55189a0
mscaldas2012 <mscaldas2012>
2023-04-26 20:51:25 +0000

    fix merge issues, checkpoint of lake-segs

--- pipeline/silver/hl7-lake-segments-ok-silver.py
+++ pipeline/silver/hl7-lake-segments-ok-silver.py
@@ -60,11 +60,6 @@ df1 = lake_util.read_stream_from_table()
 df2 = df1.drop("summary", "metadata_version", "processes", "status", "process_name", "process_version", "start_processing_time", "end_processing_time") \
         .withColumnRenamed("report", "lake_segments_string")
 
-<<<<<<< HEAD
-# display( df2 )
-=======
-#display( df2 )
->>>>>>> origin/develop
 
 # COMMAND ----------
 
@@ -82,11 +77,7 @@ df3 = df2.withColumn( "lake_segments_arr", from_json( col("lake_segments_string"
          .withColumn("parent_segments", col('segment_struct.parent_segments')) \
          .drop("segment_struct")
 
-<<<<<<< HEAD
-# display( df3 )
-=======
-#display( df3 )
->>>>>>> origin/develop
+
 
 # COMMAND ----------
 
